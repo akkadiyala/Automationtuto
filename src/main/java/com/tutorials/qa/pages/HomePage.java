bf2ad93c13aba1f1fd1c00f1f09f41e6d33620af
akkadiyala <148742089+akkadiyala@users.noreply.github.com>
2023-11-01 10:29:14 +0530

    Update HomePage.java

--- src/main/java/com/tutorials/qa/pages/HomePage.java
+++ src/main/java/com/tutorials/qa/pages/HomePage.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
+//just a message to check in jenkins with git repo after changes 
 public class HomePage {
 	WebDriver driver;
 	@FindBy(xpath= "//span[text()='My Account']")
